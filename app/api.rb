aaac3d3f677b53a40a506524a8c9f597a14aeaa2
dluman <dluman@allegheny.edu>
2024-07-22 21:32:11 -0400

    Updating with EOD-I-sleep-now progress

--- app/api.rb
+++ app/api.rb
@@ -1,16 +1,39 @@
 # Gemfile libraries
 require 'sinatra'
+require 'fileutils'
+require 'securerandom'
 require 'require_all'
+require 'include_all'
 
 # Internal operations libraries
 require_all 'methods'
-include Oulipean
+include_all
+
+def write_resource(content)
+  FileUtils.makedir_p 'resources'
+  name = securerandom.hex
+  begin
+    File.open("resources/#{name}", "w") {
+      |file| file.write(content)
+    }
+  rescue => e
+    false
+  end
+  true
+end
+
+def acquire_content(data)
+  data.body.rewind
+  body = JSON.parse data.read
+  puts body
+end
 
 # Process calls to valid endpoints; fence out
 # all of the bad requests
 post '/:category?/:operation?' do
   category = params[:category]
   operation = params[:operation]
+  puts JSON.parse request.body.read
   begin
     response = :category.public_send(operation, "HI")
     status 200
