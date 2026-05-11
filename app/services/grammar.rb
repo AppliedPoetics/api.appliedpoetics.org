60302b5136df5d1dd7f63a2b2247052b3d6a895b
dluman <dluman@allegheny.edu>
2026-05-11 11:30:54 -0400

    Adding dependencies and additional Grammar menu items

--- app/services/grammar.rb
+++ app/services/grammar.rb
@@ -22,3 +22,30 @@ class Isolator < Grammar
         params[:text].split(/[#{all_punct}]/).select { |s| s.last == desired_punct }.join(" ")
     end
 end
+
+class Quotations < Grammar
+    def self.create(params)
+        params[:text].scan(/"([^"]*)"/).flatten.join(" ")
+    end
+end
+
+class PartsOfSpeech < Grammar
+    VALID_TAGS = %w[
+        adjectives adverbs base_present_verbs comparative_adjectives
+        conjunctions gerund_verbs infinitive_verbs interrogatives
+        max_noun_phrases noun_phrases nouns passive_verbs past_tense_verbs
+        present_verbs proper_nouns question_parts superlative_adjectives
+        verbs words
+    ].freeze
+
+    def self.create(params)
+        part_of_speech = params.fetch(:tag)
+        unless VALID_TAGS.include?(part_of_speech)
+            raise ArgumentError, "Invalid part of speech: #{part_of_speech}. Valid tags: #{VALID_TAGS.join(', ')}"
+        end
+        require 'engtagger'
+        tgr = EngTagger.new
+        tagged = tgr.add_tags(params[:text])
+        tgr.public_send("get_#{part_of_speech}", tagged).keys.join(" ")
+    end
+end
