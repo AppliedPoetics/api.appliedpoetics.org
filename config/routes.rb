8e41895156d1124172cd0f8ab75993768daa919e
dluman <dluman@allegheny.edu>
2026-05-13 16:14:00 -0400

    Adding docs build; need to preview locally

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+    # Serve Swagger UI at root
+    mount Rswag::Ui::Engine => "/"
+    # Serve OpenAPI specs
+    mount Rswag::Api::Engine => "/api-docs"
+
     namespace :v1 do
         post "mcp", to: "mcp#create"
         scope "/:cat" do
